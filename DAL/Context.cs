ca8c28190b2d48f66a1c72fe15774eb9c7c99658
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2024-09-23 21:41:55 -0400

    Creando el contexto.

---
+++ DAL/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using JendriHidalgo_Ap1_P1.Models;
+
+namespace JendriHidalgo_Ap1_P1.DAL
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> option) : base(option)
+        {
+        }
+
+        public DbSet<Registro> Registro { get; set; }
+    }
+}
