2c495d7c6e0d42515a73ad89e766dc10f76b46ec
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2024-09-23 21:42:07 -0400

    Creando el modelo.

---
+++ Models/Registro.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JendriHidalgo_Ap1_P1.Models
+{
+    public class Registro
+    {
+
+        [Key]
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
