f0d0b42f40edd491e71067a8dddd0663339524ad
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2024-09-30 18:43:15 -0400

    Creando los modelos.

---
+++ Models/Prestamo.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JendriHidalgo_Ap1_P1.Models
+{
+    public class Prestamo
+    {
+
+        [Key]
+
+        public int DeudorId { get; set; }
+
+        [Required(ErrorMessage = "El campo Deudor es obligatorio")]  
+
+        public string Deudor { get; set; }
+
+        [Required(ErrorMessage = "El campo Concepto es obligatorio")]
+        public string Concepto { get; set; }
+
+        public int Monto { get; set; }
+
+    }
+}
