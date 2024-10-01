12bea314f0b102e1ad556fe15105ced4394adef3
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2024-09-30 21:48:50 -0400

    Haciendo las inyecciones.

--- Program.cs
+++ Program.cs
@@ -3,36 +3,32 @@ using JendriHidalgo_Ap1_P1.DAL;
 using JendriHidalgo_Ap1_P1.Services;
 using Microsoft.EntityFrameworkCore;
 
-
-
-
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 var ConStr = builder.Configuration.GetConnectionString("ConStr");
 builder.Services.AddDbContext<Context>(c => c.UseSqlite(ConStr));
 
-builder.Services.AddScoped<RegistroService>();
+builder.Services.AddScoped<PrestamoService>();
+
+// Add services to the container.
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
-app.UseAntiforgery();
+app.UseRouting();
 
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+app.UseAuthorization();
+
+app.MapRazorPages();
 
 app.Run();
